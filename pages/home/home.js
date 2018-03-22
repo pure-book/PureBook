f61e895220b41e8a474e19015349f03e4cf56725
陈淇格 <cqg186495@outlook.com>
2018-03-22 19:00:37 +0800

    静态首页，作者页面

--- pages/home/home.js
+++ pages/home/home.js
@@ -3,6 +3,18 @@ const app = getApp()
 Page({
   onLoad:function(){
     
+  },
+
+  jumpAuthor:function(){
+    wx.navigateTo({
+      url:"../author/author"
+    })
+  },
+  
+  jumpBook:function(){
+    wx.navigateTo({
+      url:"../book/book"
+    })
   }
 
 })
