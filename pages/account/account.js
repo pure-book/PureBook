84db0bdfabe4f100ba0d2e3b7d3695753e0911dd
陈淇格 <cqg186495@outlook.com>
2018-03-29 14:38:14 +0800

    手动合并分支Akira

--- pages/account/account.js
+++ pages/account/account.js
@@ -1,5 +1,15 @@
 const app = getApp()
 
 Page({
-  
+  gotolike: function () {
+    wx.navigateTo({
+      url: '../like/like',
+    })
+  },
+  gotoread: function () {
+    wx.navigateTo({
+      url: '../read/read',
+    })
+  }
 })
+
