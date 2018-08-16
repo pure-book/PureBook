4f035d924c1433bf356a216b3ddd741684e98af2
ChrisChen <chris.chen05@sap.com>
2018-08-16 11:11:59 +0800

    app.js

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-//app.js
 App({
     onLaunch: function() {
-        var that = this;
-        // 展示本地存储能力
         var logs = wx.getStorageSync('logs') || []
         logs.unshift(Date.now())
         wx.setStorageSync('logs', logs)
