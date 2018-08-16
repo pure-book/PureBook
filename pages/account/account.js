1a6b758449ba0fa6e81e60e40cdb08657ab2a9e0
ChrisChen <chris.chen05@sap.com>
2018-08-16 10:49:59 +0800

    add new feature

--- pages/account/account.js
+++ pages/account/account.js
@@ -1,44 +1,38 @@
 const app = getApp()
 var http = require('../../utils/httpUtil.js')
-var api = ""
-var params={
-
-}
-
 Page({
-  data:{
-    userInfo:{}
-  },
+    data: {
+        userInfo: {}
+    },
 
-  onLoad:function(){
-    var that = this
-    app.getUserInfo(function(userInfo){
-      that.setData({
-        userInfo:userInfo
-      })
-    })
+    onLoad: function() {
+        var that = this
+        app.getUserInfo(function(userInfo) {
+            that.setData({
+                userInfo: userInfo
+            })
+        })
 
-  },
+    },
 
-  gotolike: function () {
-    wx.navigateTo({
-      url: '../like/like',
-    })
-  },
-  gotomyBooklist: function () {
-    wx.navigateTo({
-      url: '../myBooklist/myBooklist',
-    })
-  },
-  gotomyReview: function () {
-    wx.navigateTo({
-      url: '../myReview/myReview',
-    })
-  },
-  gotomysetting: function () {
-    wx.navigateTo({
-      url: '../setting/setting',
-    })
-  }
+    gotolike: function() {
+        wx.navigateTo({
+            url: '../like/like',
+        })
+    },
+    gotomyBooklist: function() {
+        wx.navigateTo({
+            url: '../myBooklist/myBooklist',
+        })
+    },
+    gotomyReview: function() {
+        wx.navigateTo({
+            url: '../myReview/myReview',
+        })
+    },
+    gotomysetting: function() {
+        wx.navigateTo({
+            url: '../setting/setting',
+        })
+    }
 })
-
