e7bbc5fffd8b432715be5650d3113ca2e5e5e902
ChrisChen <chris.chen05@sap.com>
2018-08-17 16:31:50 +0800

    app

--- app.js
+++ app.js
@@ -84,7 +84,6 @@ App({
                     wx.getUserInfo({
                         success: function(res) {
                             that.globalData.userInfo = res.userInfo
-
                             typeof cb == "function" && cb(that.globalData.userInfo);
                         }
                     })
