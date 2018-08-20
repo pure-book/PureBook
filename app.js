374ef510db8dc04caee30c7b3cfbd374ab3059c3
ChrisChen <chris.chen05@sap.com>
2018-08-20 14:20:23 +0800

    no message

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ App({
                                 success: function(data) {
                                     if (data.data.status == 1) {
                                         let userInfo_ = data.data.userInfo;
-
                                         id = userInfo_.openId;
                                         that.globalData.userId = id;
                                         console.log(that.globalData.userId);
